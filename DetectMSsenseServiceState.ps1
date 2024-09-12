def47ac07aa1e7fcfd6f7076ef7731cd4086d157
Per Larsen <pelarsen@microsoft.com>
2024-09-12 14:16:11 +0200

    Add files via upload

---
+++ DetectMSsenseServiceState.ps1
@@ -0,0 +1,50 @@
+﻿#=============================================================================================================================
+#
+# Script Name:     DetectMSsenseServiceState.ps1
+# Description:     Purpose of this script is to detect if MSsense feature is enabled and further if "MSSense" is running
+# Notes:           No variable substitution should be necessary
+#
+#=============================================================================================================================
+
+# Define Variables
+$svcCur = "Sense"
+
+# Main script
+   
+   
+If ($(Get-Service -Name "Sense" -ErrorAction SilentlyContinue).Status) {
+ 
+	Write-Output -InputObject "Service Exist"
+  }
+    Else{
+        Write-Error "Error: " + $errMsg
+        exit 1
+ }
+
+Try{        
+    $svcCTRSvc = Get-Service $svcCur
+    $curSvcStat = $svcCTRSvc.Status
+}
+
+Catch{    
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
+
+If ($curSvcStat -eq "Running"){
+    Write-Output $curSvcStat
+    exit 0                        
+}
+Else{
+    If($curSvcStat -eq "Stopped"){
+        Write-Output $curSvcStat
+        exit 1     
+    }
+    Else{
+        Write-Error "Error: " + $errMsg
+        exit 1
+    } 
+
+} 
+
---
+++ RemediateMSsenseServiceInstalled.ps1
@@ -0,0 +1,36 @@
+﻿#=============================================================================================================================
+#
+# Script Name:     RemediateMSsenseServiceInstalled.ps1
+# Description:     Purpose of this script is to enable MSsense feature.
+#       Notes:     No variable substitution needed             
+#
+#=============================================================================================================================
+
+# Define Variables
+$svcCur = "Sense1"
+
+# We'll going to install the capability.
+Try{        
+    If ($(Get-Service -Name "Sense" -ErrorAction SilentlyContinue).Status) {
+ 
+	Write-Output -InputObject "Service Exist"
+    }
+  }
+Catch{    
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    Exit 1
+    }
+      
+# Okay, We'll going to install the capability
+Try{        
+Add-WindowsCapability -Online -Name "Microsoft.Windows.Sense.Client~~~~"    
+     
+    }
+
+Catch{    
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    Exit 1
+    }
+  
